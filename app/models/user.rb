78fd92785f43c02774730fed6fd5817dd38664a0
Faizan Haider <hfaizan53@gmail.com>
2024-09-17 16:14:13 +0500

    Login and sign-up added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password_length, length: {minimum: 8}, if: password_length.present?
 end
